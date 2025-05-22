65389353d7fd916b985c85ce813f93611aab779b
Thomas James <wjamesthomas3@gmail.com>
2025-05-22 19:53:56 -0400

    Added git aliases and header for rmd.sh

--- commands/rmd/rmd.sh
+++ commands/rmd/rmd.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rmd() {
   # Check argument exists
   if [ $# -eq 0 ]; then
--- preferences/aliases.sh
+++ preferences/aliases.sh
@@ -11,6 +11,13 @@ alias ls='logo-ls -1 -D'
 alias lsa='logo-ls -1 -A -D'
 alias l.='logo-ls -i -a -1 -D | grep "^\."'
 
+# Git Aliases
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -m"
+alias gp="git push"
+alias gl="git log --oneline --graph"
+
 # Safety Flags
 alias mv='mv -iv' # Prompt about moving files/directories
 alias cp='cp -iv' # Prompt about copying files/directories
